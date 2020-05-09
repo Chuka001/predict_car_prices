9284cfb7db3f8883f3232543fa602b03a484bd56
Chuka001 <ijeoma.e.chukwuka@gmail.com>
2020-05-09 02:37:49 -0700

    Update fetch_kaggle_dataset.sh
    Modified shell script

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kaggle competitions download -c Used-Cars-Dataset -p packages/regression_model/regression_model/datasets/
+kaggle competitions download -c craigslist-carstrucks-data -p packages/regression_model/regression_model/datasets/
