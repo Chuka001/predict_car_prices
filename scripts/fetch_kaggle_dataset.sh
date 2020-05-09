fbeeba12f50755527302c05f50cd08f62ea47181
Chuka001 <ijeoma.e.chukwuka@gmail.com>
2020-05-09 03:06:54 -0700

    Update fetch_kaggle_dataset.sh

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-!kaggle datasets download -d austinreese/craigslist-carstrucks-data -p packages/regression_model/regression_model/datasets/
+kaggle datasets download -d austinreese/craigslist-carstrucks-data -p packages/regression_model/regression_model/datasets/
